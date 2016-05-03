984bc039daa3a7aaa7abee9b4847bd4d217f8f68
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-05-03 14:03:26 +0100

    Add version of the backup

--- copiaficheros.sh
+++ copiaficheros.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
+number_of_files=$(ls | grep $1-$(date +%y%m%d) | nl -n ln | cut -d " " -f1 | tail -1)
+
 cat "./files.txt" | grep "/" | while read line
 do	
-	tar -rpvf $1-$(date +%y%m%d).tar $line
+	tar -rpvf $1-$(date +%y%m%d)$number_of_files.tar $line
 done
