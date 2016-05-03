ef8c112d0f2b9f1c02c7bf2640b4fea07f9c5e98
David Falcón Naranjo <david_el_125@hotmail.com>
2016-05-03 14:09:02 +0100

    File path list existence now is cheked

--- copiaficheros.sh
+++ copiaficheros.sh
@@ -1,6 +1,9 @@
-#!/bin/bash 
-
-cat "./files.txt" | grep "/" | while read line
+#!/bin/bash
+file="./files.txt"
+if [ ! -f $file ]; then 	
+    echo "#insert files path#" >> $file
+fi
+cat $file | grep "/" | while read line
 do	
 	tar -rpvf $1.tar $line
 done
