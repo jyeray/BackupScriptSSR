c07a10e7d8cc2e8d339995998352e4d317425f4f
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-05-04 20:36:45 +0100

    Fix bug.

--- backup_script.sh
+++ backup_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 file="./backup_file_paths"
-if [ ! -f $file ]; then 	
+if [ ! -f $file ]; then
     echo "#--insert file path--#" >> $file
 fi
-cat $file | grep "/" | while read line
 number_of_files=$(ls | grep $1-$(date +%y%m%d) | nl -n ln | cut -d " " -f1 | tail -1)
-do	
+cat $file | grep "/" | while read line
+do
 	tar -rpvf $1-$(date +%y%m%d)$number_of_files.tar $line
 done
